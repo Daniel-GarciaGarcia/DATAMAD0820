6bd3942b6d391e8f4656634fe5fd405d3d4d1eaf
Daniel-GarciaGarcia <kasiya93@gmail.com>
2020-08-20 17:37:01 +0200

    TeDeLasCinco

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,66 +1,80 @@
 #Example: 
-
+'''
 eggs = (1,3,8,3,2)
 
 my_listComprehension = [1/egg for egg in eggs]
 
 print(my_listComprehension)
-
+'''
 #Insert here the module/library import statements 
 
-
+import math
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+'''
+sqnmb= []
+sqnmb = [i*i for i in range(1,21)] 
+print(sqnmb)'''
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
-
+'''
+power_of_two=[]
+power_of_two= [2**i for i in range(1,51)] 
+print(power_of_two)
+'''
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
-
-
-
-
+'''
+sqrt= []
+sqrt= [i**0.5 for i in range(0,101)]
+print (sqrt)
+'''
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+'''
+my_list=[]
+my_list=[i for i in range (-10,1)]
+print(my_list)
+'''
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odd= []
+odd = [i for i in range(0,101) if  i%!==1]
+print (odd)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven=  []
+divisible_by_seven = [i//7==0 for i in range (0,1001)]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
-
+'''
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
-
-
+vowels= 'aeiou'
+non_vowels= []
+non_vowels= [i for i in teststring if not in 'aeiou']
+print(non_vowels)
+'''
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+Lazy='The Quick Brown Fox Jumped Over The Lazy Dog'
+
+Capital_letters=[]
+
+print(Capital_letters)
 
 
 
@@ -68,8 +82,10 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+Lazy='The Quick Brown Fox Jumped Over The Lazy Dog'
 
-
+aConso=[]
+print(aConso)
 
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
@@ -78,7 +94,8 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 
 
-#11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
+#11. Create 4 lists of 10 random numbers between 0 and 100 each. Use ran
+'''dom_lists as the name of the list.''' 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
@@ -102,33 +119,38 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 
 
+
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
-
-for i in ['a','b','c']:
-    print i**2
+'''for i in ['a','b','c']:
+    try:
+        print ('Ponle parentesis a todo print!')
+        print i**2
+    except:
+        print (i**2)'''
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
 #Then use a finally block to print 'All Done.'
 # Check in provided resources the type of error you may use. 
-
+'''
 x = 5
 y = 0
 
-z = x/y
+z = x/y'''
 
 
 
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
-
+'''
 abc=[10,20,20]
-print(abc[3])
+print(abc[3])'''
 
 
-#17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
+#17. Handle at least two kind of different exceptions when d
+'''ividing a couple of numbers provided by the user. '''
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
 
@@ -136,20 +158,20 @@ print(abc[3])
 
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
-# Check in provided resources the type of error you may use. 
-
+# Check in provided resources the type odivisible_by_sevenf error you may use. 
+'''
 f = open('testfile','r')
 f.write('Test write this')
-
+'''
 
 
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
-
+'''
 fp = open('myfile.txt')
     line = f.readline()
-    i = int(s.strip())
+    i = int(s.strip())'''
 
 
 
