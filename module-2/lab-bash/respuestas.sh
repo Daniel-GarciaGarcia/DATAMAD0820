c0716712674ef8ab2b9d02160c418f5bd5bcef65
Daniel-GarciaGarcia <kasiya93@gmail.com>
2020-09-01 19:59:10 +0200

    bashazo

---
+++ module-2/lab-bash/respuestas.sh
@@ -0,0 +1,20 @@
+1- print "hello World"
+2- mkdir new_dir
+3- rmdir new_dir
+4-cp sed.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem-copy
+5-cp at.txt lorem.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem-copy
+6- cat.sed.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem
+7- cat at.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem
+7- cat lorem.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem
+8- head -n 3 sed.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem-copy
+9- tail -n 3 sed.txt /home/danielgarcia/datamad0820/module-2/lab-bash/lorem-copy
+10- echo 'Homo homini lupus.'>>sed.txt 
+11- sed 's/et/ET/g' at.txt
+12 -whoami
+13 pwd
+14-ls *.txt
+15-wc -l  lorem/sed.txt
+16- ls lorem*  | wc -l
+17-grep -o -i et | wc -l lorem/at.txt
+18-
+19 - grep -ri "et" lorem-copy
